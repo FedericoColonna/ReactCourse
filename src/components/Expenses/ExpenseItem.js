3bcf098ae16e994e08c7cfa92823e083195036f4
Federico Colonna <federico.colonna89@gmail.comn>
2022-12-13 11:01:51 +0100

    State and events updated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import Card from '../UI/Card';
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
- 
+
+    const [title, setTtitle] = useState(props.title);
+
+    
+
+  const clickHandler = () => {
+    setTtitle('Updated!');
+  };
 
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date}/>
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}$</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.amount}$</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
